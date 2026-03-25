479a6de349d1d411aa6b1ca186c07cec52416280
Thomas Evensen <thomeven@gmail.com>
2026-03-25 11:02:06 +0100

    Update rawcull.rb

--- Casks/rawcull.rb
+++ Casks/rawcull.rb
@@ -2,7 +2,7 @@ cask "rawcull" do
   version "1.2.3"
   sha256 "75774876f2a049a9131417e34d62d09e148262046384c6b3681465730af8d990"
  
-  url "https://github.com/rsyncOSX/RawCull/releases/download/v#{version}/RawCull.#{version}.dmg"
+  url "https://github.com/rsyncOSX/RawCull/releases/v#{version}/RawCull.#{version}.dmg"
   name "RawCull"
   desc "Cull large sets of Sony ARW raw photos"
   homepage "https://github.com/rsyncOSX/RawCull"
