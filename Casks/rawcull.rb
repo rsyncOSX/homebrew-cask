b18d50420f9f6ebc0e46b34a88b00d22c108b8c5
Thomas Evensen <thomeven@gmail.com>
2026-03-25 11:54:51 +0100

    Tidy RawCull cask: reorder zap paths
    Move the ~/Library/Preferences/com.rsyncOSX.RawCull.plist entry after the Application Support path in the zap trash array and remove extraneous blank lines for cleaner formatting. This is a non-functional cleanup to improve consistency in the cask file.

--- Casks/rawcull.rb
+++ Casks/rawcull.rb
@@ -1,7 +1,7 @@
 cask "rawcull" do
   version "1.2.3"
   sha256 "75774876f2a049a9131417e34d62d09e148262046384c6b3681465730af8d990"
- 
+
   url "https://github.com/rsyncOSX/RawCull/releases/download/v#{version}/RawCull.#{version}.dmg"
   name "RawCull"
   desc "Cull large sets of Sony ARW raw photos"
@@ -9,11 +9,11 @@ cask "rawcull" do
 
   depends_on macos: ">= :tahoe"
   depends_on arch: :arm64
- 
+
   app "RawCull.app"
- 
+
   zap trash: [
-    "~/Library/Preferences/com.rsyncOSX.RawCull.plist",
     "~/Library/Application Support/RawCull",
+    "~/Library/Preferences/com.rsyncOSX.RawCull.plist",
   ]
 end
