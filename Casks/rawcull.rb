5554d2db9450fc421307870f2d98684c630cd5a1
Thomas Evensen <thomeven@gmail.com>
2026-04-04 06:47:16 +0200

    Update rawcull.rb

--- Casks/rawcull.rb
+++ Casks/rawcull.rb
@@ -1,6 +1,6 @@
 cask "rawcull" do
-  version "1.3.4"
-  sha256 "36d5ad9ce95e4fccc1c44245615ccd9514f1d362924be1b5e75fea43f282cd48"
+  version "1.3.5"
+  sha256 "cb9388f9992719370848cb4d8d76a065b812951e0dd5e67692b5e65e664c1895"
 
   url "https://github.com/rsyncOSX/RawCull/releases/download/v#{version}/RawCull.#{version}.dmg"
   name "RawCull"
