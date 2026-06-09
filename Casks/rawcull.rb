21f31c4283a70915f4e4f7ef2febc9a76c1a8a17
Thomas Evensen <thomeven@gmail.com>
2026-06-09 09:55:12 +0200

    update

--- Casks/rawcull.rb
+++ Casks/rawcull.rb
@@ -7,7 +7,7 @@ cask "rawcull" do
   desc "Cull large sets of Sony ARW RAW photos"
   homepage "https://github.com/rsyncOSX/RawCull"
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: :tahoe
   depends_on arch: :arm64
 
   app "RawCull.app"
