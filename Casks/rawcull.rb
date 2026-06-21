49f4da8361231abd90a19223047783e67afae544
Thomas Evensen <thomeven@gmail.com>
2026-06-21 10:03:20 +0200

    new version

--- Casks/rawcull.rb
+++ Casks/rawcull.rb
@@ -1,6 +1,6 @@
 cask "rawcull" do
-  version "2.1.6"
-  sha256 "2b7361c867765538154d388eeafd4ba728cc14b03e08ece04d2e0c5b851aeb77"
+  version "2.1.7"
+  sha256 "e5b2d8dfab24f2ad7fd39ec59afc83aacf9d6100ebf05fba712fa70dc9b24039"
 
   url "https://github.com/rsyncOSX/RawCull/releases/download/v#{version}/RawCull.#{version}.dmg"
   name "RawCull"
